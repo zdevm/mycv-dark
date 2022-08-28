e31ced9c3a158cacb78505bb8fb4b34d55117e65
Marios Zafeiriou <zafeidev@gmail.com>
2022-08-29 01:01:09 +0300

    change default favicon && fetch and set favicon from back end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
-import { Profile } from '@interfaces/profile';
 import { LoadingScreenService } from '@services/loading-screen/loading-screen.service';
 import { ProfileService } from '@services/profile/profile.service';
 
@@ -11,12 +11,36 @@ import { ProfileService } from '@services/profile/profile.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private readonly faviconId = 'mainFavicon';
+
   public constructor(private readonly router: Router,
                      private readonly loadingScreenService: LoadingScreenService,
                      private readonly profileService: ProfileService,
                      private readonly titleService: Title) {
     this.listenRouter();
     this.loadFullNameAndSetTitle();
+    this.updateFavicon();
+  }
+
+  /**
+   * Checks if back end server has available favicon. If so, updates default favicon.
+   */
+  private updateFavicon() {
+    const http = inject(HttpClient);
+    http.get('favicon.ico', {
+      observe: 'response',
+      responseType: 'blob'
+    }).subscribe({
+      next: response => { 
+        if (response.status === 200 && response.body?.size) { // favicon exists
+          const blobUrl = window.URL.createObjectURL(response.body)
+          const faviconElem = document.getElementById(this.faviconId);
+          if (faviconElem && blobUrl) {
+            faviconElem.setAttribute('href', blobUrl);
+          }
+        }
+      }
+    })
   }
 
   private loadFullNameAndSetTitle() {
